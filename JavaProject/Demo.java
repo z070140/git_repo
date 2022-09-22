1b1b36464f6d88cacc65446c429bd579e7414a9f
z070140 <z070140@gmail.com>
2022-09-22 11:00:09 +0800

    add java

---
+++ JavaProject/Demo.java
@@ -0,0 +1,7 @@
+public class Demo {
+    public static void main(String[] args) {
+        //double d;
+        boolean t = Boolean.parseBoolean("true");
+        System.out.println(t);
+    }
+}
---
+++ JavaProject/Scan.java
@@ -0,0 +1,12 @@
+public class Scan {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 2;
+        y = ++x;
+        System.out.println(x + y);
+        boolean yes;
+        //yes = (y % 2 != 1);
+        yes = ( y % 2 == 0);
+        System.out.println(yes);
+    }
+}
