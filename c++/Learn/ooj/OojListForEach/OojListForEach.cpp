3b129328bf73a28fc92d9c00f9630ef55229dfaa
z070140 <z070140@gamil.com>
2022-11-05 19:58:18 +0800

    C++ OOJ

---
+++ c++/Learn/ooj/OojListForEach/OojListForEach.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Value {
+private:
+	int i;
+public:
+	void set(int i) {
+		this->i = i;
+	}
+	int get() {
+		return i;
+	}
+};
+
+int main() {
+	Value a[10];
+	cout <<  << endl;
+	for (int i = 0; i < sizeof(a)/sizeof(a[0]); i++) {
+		
+
+	}
+
+}
--- c++/Learn/ooj/notebook/notebook/NoteBook.cpp
+++ c++/Learn/ooj/notebook/notebook/NoteBook.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #include<string>
 #include <vector>
+#include<exception>
 
 class NoteBook {
 private:
@@ -10,9 +11,11 @@ private:
 public:
 	void add(string s){
 		notes.push_back(s);
+		//cout << sizeof(notes) << endl;
 	}
 	void add(string s, int location) {
 		notes.insert(notes.begin() + location, s);
+		//cout << sizeof(notes)<<endl;
 	}
 	int getSize() {
 		return notes.size();
@@ -21,36 +24,57 @@ public:
 		return notes[index];
 	}
 	void removeNote(int index) {
-		try{
 		notes.erase(notes.begin()+index);
-		}
-		catch(exception e) {
-			cout << "异常！越界！" << endl;
-		}
-
+		
 	}
+	//string* list() {
+	//	string *p= new string[(int)notes.size()];
+	//	for (int i = 0; i < notes.size(); i++) {
+	//		p[i] = notes[i];
+	//	}
+	//	return p;
+	//}
+
 	string* list() {
-		string* p= new string[(int)notes.size()];
-		for (int i = 0; i < notes.size(); i++) {
-			p[i] = notes[i];
-		}
+		
+		string* p = new string[(int)notes.size()];
+		memcpy(p, &notes[0], notes.size()*sizeof(notes[0]));
 		return p;
 	}
+
 };
 
 int main() {
-	//string a[2];
-	//a[0] = "first";
-	//a[1] = "second";
-	NoteBook nb;
-	nb.add("first");
-	nb.add("second");
-	nb.add("third", 1);
-	//cout << nb.getNote(1)<<endl;
-	nb.removeNote(1);
-	string* p = nb.list();
-	for (int i = 0;i< nb.getSize(); i++) {
-		cout << p[i] << endl;
+	string *a[10];
+	cout << a[0] << endl;
+	cout << a[0].length() << endl;
+	string s = "";
+	cout << s.length() << endl;
+	for (char i =49; i <sizeof(a)/sizeof(a[0]); i++) {
+		a[i]=i;
 	}
+	cout << a[0] << endl;
+	cout << a[0].length() << endl;
+	////string a[2];
+	////a[0] = "first";
+	////a[1] = "second";
+	//NoteBook nb;
+	//nb.add("first");
+	//nb.add("second");
+	//nb.add("third", 1);
+	//
+	////cout << nb.getNote(1)<<endl;
+	//nb.removeNote(1);
+	//string* p = nb.list();
+	//for (int i = 0;i< nb.getSize(); i++) {
+	//	cout << p[i] << endl;
+	//}
+	////string a = "";
+	////cout << sizeof(a)<<endl;
+	////string b = "我是有数据的啊";
+	////cout << sizeof(b)<<endl;
+
+	//string a[10];
+	//cout << a[0] << endl;
 	return 0;
 }
